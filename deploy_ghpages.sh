897ae477615c285db45918acbf48f5f67678a818
Adrien LAUER <adrien.lauer@gmail.com>
2016-04-22 16:37:27 +0200

    Add missing license header

--- deploy_ghpages.sh
+++ deploy_ghpages.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+#
+# Copyright (c) 2013-2016, The SeedStack authors <http://seedstack.org>
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+
 
 (
     git clone https://github.com/seedstack/smoke-tests.git target/gh-pages
